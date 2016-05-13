4524214b54c0fbfa23b199e4090fbded2e40d3ef
Sebastian Mancke <s.mancke@tarent.de>
2016-05-13 23:43:02 +0200

    small refactoring

--- composition/content_fetcher.go
+++ composition/content_fetcher.go
@@ -24,7 +24,26 @@ type FetchDefinition struct {
 func NewFetchDefinition(url string) *FetchDefinition {
 	return &FetchDefinition{
 		URL:            url,
-		Timeout:        10*time.Second,
+		Timeout:        10 * time.Second,
+		RequestHeaders: nil,
+		Required:       true,
+	}
+}
+
+// NewFetchDefinitionFromRequest creates a fetch definition
+// from the request path, but replaces the sheme, host and port with the provided base url
+func NewFetchDefinitionFromRequest(baseUrl string, r *http.Request) *FetchDefinition {
+	fullPath := r.URL.Path
+	if fullPath == "" {
+		fullPath = "/"
+	}
+	if r.URL.RawQuery != "" {
+		fullPath += "?" + r.URL.RawQuery
+	}
+
+	return &FetchDefinition{
+		URL:            baseUrl + fullPath,
+		Timeout:        10 * time.Second,
 		RequestHeaders: nil,
 		Required:       true,
 	}
