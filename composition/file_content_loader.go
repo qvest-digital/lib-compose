43cd04b67622a2a73cc907b093940599b926a632
Sebastian Mancke <s.mancke@tarent.de>
2016-06-19 14:33:47 +0200

    implemented file content loader for file protocol

---
+++ composition/file_content_loader.go
@@ -0,0 +1,51 @@
+package composition
+
+import (
+	"errors"
+	"fmt"
+	"github.com/tarent/lib-compose/logging"
+	"os"
+	"strings"
+	"time"
+)
+
+var RequestProcessorsNotApplicable = errors.New("request processors are not apliable on file content")
+
+type FileContentLoader struct {
+	parser ContentParser
+}
+
+func NewFileContentLoader() *FileContentLoader {
+	return &FileContentLoader{
+		parser: &HtmlContentParser{},
+	}
+}
+
+func (loader *FileContentLoader) Load(fd *FetchDefinition) (Content, error) {
+	if fd.RespProc != nil {
+		return nil, RequestProcessorsNotApplicable
+	}
+	filename := strings.TrimPrefix(fd.URL, FileURLPrefix)
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("error opening file %v: %v", fd.URL, err)
+	}
+
+	c := NewMemoryContent()
+	c.url = fd.URL
+	c.httpStatusCode = 200
+
+	if strings.HasSuffix(filename, ".html") {
+		parsingStart := time.Now()
+		err := loader.parser.Parse(c, f)
+		logging.Logger.
+			WithField("full_url", c.URL()).
+			WithField("duration", time.Since(parsingStart)).
+			Debug("content parsing")
+		f.Close()
+		return c, err
+	}
+
+	c.reader = f
+	return c, nil
+}
---
+++ composition/file_content_loader_test.go
@@ -0,0 +1,81 @@
+package composition
+
+import (
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+func Test_FileContentLoader_LoadHTML(t *testing.T) {
+	a := assert.New(t)
+
+	fileName := filepath.Join(os.TempDir(), randString(10)+".html")
+
+	err := ioutil.WriteFile(fileName, []byte("<html><head>some head content</head></html>"), 0660)
+	a.NoError(err)
+
+	loader := NewFileContentLoader()
+	c, err := loader.Load(NewFetchDefinition(FileURLPrefix + fileName))
+	a.NoError(err)
+	a.NotNil(c)
+	a.Nil(c.Reader())
+	a.Equal(FileURLPrefix+fileName, c.URL())
+	eqFragment(t, "some head content", c.Head())
+	a.Equal(0, len(c.Body()))
+}
+
+func Test_FileContentLoader_LoadStream(t *testing.T) {
+	a := assert.New(t)
+
+	fileName := filepath.Join(os.TempDir(), randString(10)+".css")
+
+	err := ioutil.WriteFile(fileName, []byte("some non html content"), 0660)
+	a.NoError(err)
+
+	loader := NewFileContentLoader()
+	c, err := loader.Load(NewFetchDefinition(FileURLPrefix + fileName))
+	a.NoError(err)
+	a.NotNil(c)
+	body, err := ioutil.ReadAll(c.Reader())
+	a.NoError(err)
+	a.Equal("some non html content", string(body))
+}
+
+func Test_FileContentLoader_LoadError(t *testing.T) {
+	a := assert.New(t)
+
+	loader := NewFileContentLoader()
+	_, err := loader.Load(NewFetchDefinition("/tmp/some/non/existing/path"))
+	a.Error(err)
+}
+
+func Test_FileContentLoader_RequestProcessor(t *testing.T) {
+	a := assert.New(t)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	fd := NewFetchDefinition("/tmp/some/non/existing/path")
+	fd.RespProc = NewMockResponseProcessor(ctrl)
+
+	_, err := NewFileContentLoader().Load(fd)
+	a.Equal(RequestProcessorsNotApplicable, err)
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func randString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
