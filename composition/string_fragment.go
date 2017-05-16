d3bfa7b5ab910bf63336f24bf2598239812b83a1
Christoph Borgolte <christoph.borgolte@bayard-consulting.com>
2017-05-16 09:28:26 +0200

    Removed redundant method.

--- composition/string_fragment.go
+++ composition/string_fragment.go
@@ -19,15 +19,11 @@ func (f StringFragment) Content() string {
 	return f.content
 }
 
-func (f StringFragment) ContentLength() int {
-	return len(f.content)
-}
-
 func (f StringFragment) Execute(w io.Writer, data map[string]interface{}, executeNestedFragment func(nestedFragmentName string) error) error {
 	return executeTemplate(w, f.Content(), data, executeNestedFragment)
 }
 
 // MemorySize return the estimated size in bytes, for this object in memory
 func (f StringFragment) MemorySize() int {
-	return f.ContentLength()
+	return len(f.content)
 }
